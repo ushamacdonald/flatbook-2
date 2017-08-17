eb6f357a5cce5bad78996595bc33ee35f9a28fcc
sherlyc <sherlyc@gmail.com>
2017-08-17 15:03:07 +1200

    testing submit of noticeboard

--- test/client/components/shopping.test.js
+++ test/client/components/shopping.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import test from 'ava'
 import {shallow, mount} from 'enzyme'
+import sinon from 'sinon'
 
 import '../setup-dom'
 import ShoppingListAdd from '../../../client/components/ShoppingListAdd'
@@ -15,4 +16,12 @@ test('onChange event of item input is working', (t) => {
   const event = {target: {name: 'item', value: 'word'}}
   wrapper.find('input[name="item"]').simulate('change', event)
   t.is(wrapper.state().newItem.item, 'word')
+})
+
+test('onSubmit is working', (t) => {
+  const fakeDispatch = sinon.stub()
+  const wrapper = mount(<ShoppingListAdd dispatch={fakeDispatch} match={{params: {flat_id: 1}}}/>)
+  wrapper.setState({newItem: {item: 'avocado'}})
+  wrapper.find('form').simulate('submit')
+  t.is(wrapper.state().newItem.item, 'avocado')
 })
